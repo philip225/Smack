17dc9dfca3ebb08d525245f672785102870071ee
philip <philip@wwright1.plus.net>
2020-07-16 10:00:14 +0100

    Code refactor, renamed HomeFragment fragment and ui.home package to MainFragment and ui.main package respectively (including models). Replaced deprecated ViewModelProviders with viewModels() delegate.

--- app/src/main/java/kfa/training/smack/ui/main/MainFragment.kt
+++ app/src/main/java/kfa/training/smack/ui/main/MainFragment.kt
@@ -1,4 +1,4 @@
-package kfa.training.smack.ui.home
+package kfa.training.smack.ui.main
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -6,24 +6,28 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import kfa.training.smack.R
 
-class HomeFragment : Fragment() {
+/**
+ * Course deviation - this takes the place of the MainActivity for UI operations, in effect
+ * this becomes "MainActivity".
+ */
 
-    private lateinit var homeViewModel: HomeViewModel
+
+class MainFragment : Fragment() {
+
+    private val mainViewModel: MainViewModel by viewModels()
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
         val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+        mainViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
--- app/src/main/java/kfa/training/smack/ui/main/MainViewModel.kt
+++ app/src/main/java/kfa/training/smack/ui/main/MainViewModel.kt
@@ -1,10 +1,10 @@
-package kfa.training.smack.ui.home
+package kfa.training.smack.ui.main
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class MainViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is home Fragment"
