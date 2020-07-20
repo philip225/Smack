ea45c87ac75db10006a1900a2e0e404cbd0890e7
philip <philip@wwright1.plus.net>
2020-07-20 15:13:49 +0100

    Added in AuthService registerUser. Corrected local URL call (missing port). Added in test code in CreateUserFragment.createUserClicked, to test out authentication.

--- app/src/main/java/kfa/training/smack/services/AuthService.kt
+++ app/src/main/java/kfa/training/smack/services/AuthService.kt
@@ -1,7 +1,60 @@
 package kfa.training.smack.services
 
+import android.content.Context
+import android.util.Log
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import kfa.training.smack.utilities.URL_REGISTER
+import org.json.JSONObject
+import java.nio.charset.Charset
+
 // Defined as a singleton object, since we do not want >1 instantiated AuthService.
 object AuthService {
 
+    fun registerUser(context: Context, email: String, password:String, complete:(Boolean) -> Unit){
+        /**
+         * Register a user.
+         */
+        val jsonBody = JSONObject()
+        jsonBody.put(
+            "email", email
+        )
+        jsonBody.put(
+            "password", password
+        )
+        val requestBody = jsonBody.toString()
 
+        // This part seems "messy" where it is and is really begging to be moved into a private
+        // function.
+        val registerRequest = object : StringRequest(Method.POST, URL_REGISTER, Response.Listener {
+            // This is called back on a successful request.
+            complete(true)
+        }, Response.ErrorListener {
+            Log.d("AUTH/ERROR", "Could not register user $it")
+            complete(false)
+        }){
+            // We are an anonymous class of type StringRequest which gives us certain methods we
+            // need to override.
+
+            override fun getBodyContentType(): String {
+                // This allows for the full HTTP content type where you can specify the mime and
+                // the character set.
+                // JSON character set is always UTF8, we have to specify this for Volley otherwise
+                // the arcane ISO-8859-1 character set will be used (since that is the default for
+                // HTTP protocol)!
+                return "application/json; charset=utf-8"
+            }
+
+            override fun getBody(): ByteArray {
+                // Send our body, which we have to convert to a byte array encoded to UTF8.
+                // Course deviation, I formally encode the byte array to UTF8, the course
+                // relies on the default.
+                return requestBody.toByteArray(Charset.forName("utf-8"))
+            }
+        }
+
+        // Add our request to a new request queue
+        Volley.newRequestQueue(context).add(registerRequest)
+    }
 }
--- app/src/main/java/kfa/training/smack/ui/user/CreateUserFragment.kt
+++ app/src/main/java/kfa/training/smack/ui/user/CreateUserFragment.kt
@@ -2,11 +2,13 @@ package kfa.training.smack.ui.user
 
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import kfa.training.smack.R
+import kfa.training.smack.services.AuthService
 import kfa.training.smack.utilities.toasty
 import kotlinx.android.synthetic.main.fragment_create_user.*
 import kotlin.random.Random
@@ -120,7 +122,13 @@ class CreateUserFragment : Fragment() {
 
     private fun createUserClicked(view: View){
         context?.let{
-            toasty(it, "Create user clicked.")
+            AuthService.registerUser(
+                it, "test@test.com","12345"
+            ){
+                context?.let{context ->
+                    toasty(context, "Registered use response: $it")
+                }
+            }
         }
     }
 }
