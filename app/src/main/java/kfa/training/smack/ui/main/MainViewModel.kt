175b5e8a377242959d3551916f384443a7c95748
philip <philip@wwright1.plus.net>
2020-07-30 17:52:01 +0100

    Fixed the issue with the UI updates via live data/observables and some code re-factoring.

--- app/src/main/java/kfa/training/smack/ui/main/MainViewModel.kt
+++ app/src/main/java/kfa/training/smack/ui/main/MainViewModel.kt
@@ -3,12 +3,16 @@ package kfa.training.smack.ui.main
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.recyclerview.widget.RecyclerView
 
 /* It looks like this is going to come into play! */
 class MainViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is main Fragment"
+    val mainChannelName: MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
     }
-    val text: LiveData<String> = _text
+    val listViewScroll: MutableLiveData<Int> by lazy {
+        MutableLiveData<Int>()
+    }
+
 }
